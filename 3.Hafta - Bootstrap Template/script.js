c9671f1ddc828c3cf9e523751ec79d6ad72acc56
Karya <76717341+karyaboyraz@users.noreply.github.com>
2022-04-07 21:22:23 +0300

    update

--- 3.Hafta - Bootstrap Template/script.js
+++ 3.Hafta - Bootstrap Template/script.js
@@ -1,15 +1,87 @@
 $(document).ready(function () {
-
     $('.counter').each(function () {
         $(this).prop('Counter', 0).animate({
             Counter: $(this).text()
         }, {
-            duration: 1000,
+            duration: 2000,
             easing: 'swing',
             step: function (now) {
                 $(this).text(Math.ceil(now));
             }
         });
     });
-
 });
+
+// Aos
+
+AOS.init();
+
+// back to top
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
+
+
+// type effect
+
+async function init () {
+    const node = document.querySelector("#type-text")
+    
+    await sleep(1000)
+    node.innerText = ""
+    await node.type('')
+    
+    while (true) {
+      await node.type('Uzmanlardan Eğitim Al!')
+      await sleep(3000)
+      await node.delete('Uzmanlardan Eğitim Al!')
+      await node.type('Kendini Geliştir!')
+      await sleep(3000)
+      await node.delete('Kendini Geliştir!')
+      await node.type('İşe Girme Şansı Yakala!')
+      await sleep(3000)
+      await node.delete('İşe Girme Şansı Yakala!')
+    }
+  }
+  
+  
+  // Source code 🚩
+  
+  const sleep = time => new Promise(resolve => setTimeout(resolve, time))
+  
+  class TypeAsync extends HTMLSpanElement {
+    get typeInterval () {
+      const randomMs = 100 * Math.random()
+      return randomMs < 50 ? 10 : randomMs
+    }
+    
+    async type (text) {
+      for (let character of text) {
+        this.innerText += character
+        await sleep(this.typeInterval)
+      }
+    }
+    
+    async delete (text) {
+      for (let character of text) {
+        this.innerText = this.innerText.slice(0, this.innerText.length -1)
+        await sleep(this.typeInterval)
+      }
+    }
+  }
+  
+  customElements.define('type-async', TypeAsync, { extends: 'span' })
+  
+  
+  init()
+  
